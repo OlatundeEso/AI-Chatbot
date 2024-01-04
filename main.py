bc7f325a0a2c5a40e43a6885cd3ba00dc44a3685
Olatunde Eso <eshotunde2002@yahoo.com>
2024-01-04 07:10:22 +0000

    Made adjustments to the python-dot env files

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load variables from .env file
+import os as os
+import openai as oai
+from langchain.llms import OpenAI
 
+my_llm = OpenAI(api_key=os.getenv("OPEN_AI_API_KEY"), temperature=0.6)
+
+result = my_llm("WHat is the capital of Nigeria")
+print(result)
